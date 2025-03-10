d5abcb97c94a6dd5412b4d304059c89c707c427c
Anders Höckersten <anders@hockersten.se>
2025-03-10 20:17:04 +0100

    Put tarred files at root

--- backup.sh
+++ backup.sh
@@ -2,4 +2,5 @@
 mkdir -p /tmp/backup
 sqlite3 data/db.sqlite3 "VACUUM INTO '/tmp/backup/db.sqlite3'"
 cp -a /data/attachments /data/sends /data/config.json /data/rsa_key* /data/icon_cache /tmp/backup
-tar -cjf /backup/backup-$(date '+%Y%m%d-%H%M').tar.bz2 /tmp/backup
+cd /tmp/backup
+tar -cjf /backup/backup-$(date '+%Y%m%d-%H%M').tar.bz2 *
